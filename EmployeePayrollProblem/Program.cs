107d352b64fd39ca6f5dcea61ca6faaf131fdc78
Prateek Pai <paiprateek123@gmail.com>
2022-07-29 21:52:35 +0530

    Welcome to Employee Payroll Problem using Multi Threading

---
+++ EmployeePayrollProblem/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace EmployeePayrollProblem
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Employee Payroll Problem using Multi Threading");
+        }
+    }
+}
